c62fb4d5211df1885bb6d79444345304d7d29df9
Tomasz Nowak <tn@mpro.pl>
2019-08-09 21:23:33 +0200

    add style calendar and month display

--- script/calendar.js
+++ script/calendar.js
@@ -1,32 +1,39 @@
 function calendar() {
-    var d = new Date();
-var month = d.getMonth();
-var year =d.getFullYear();
-function getDaysInMonth(month, year) {
-     var date = new Date(Date.UTC(year, month, 1));
-     var days = [];
-     while (date.getMonth() === month) {
-        days.push(new Date(date));
-        date.setDate(date.getDate() + 1);
-     }
-     return days;
-}
+   const monthNames = ["January", "February", "March", "April", "May", "June",
+      "July", "August", "September", "October", "November", "December"
+   ];
+   var d = new Date();
+   var month = d.getMonth();
+   var year = d.getFullYear();
+   var displaymonth = document.getElementById('month')
+   displaymonth.innerText += "Things ToDo in " + monthNames[d.getMonth()]
 
-var days = getDaysInMonth(month, year)
+   function getDaysInMonth(month, year) {
+      var date = new Date(Date.UTC(year, month, 1));
+      var days = [];
+      while (date.getMonth() === month) {
+         days.push(new Date(date));
+         date.setDate(date.getDate() + 1);
+      }
+      return days;
+   }
 
-days.forEach(function(el, i) {
-   var  i = ++i +0;
-   var id = 0
-   var num = `${i}` 
-   var tab = `${i}` 
-   var today = new Date()
-   var weekday = today.getDate();
-   var newdiv = '<div id='+num+' class=result> '+ tab +' </div>';
- document.getElementById('days').innerHTML += (newdiv);
- if (tab == weekday) {
- document.getElementById(weekday).classList.add('today')
- 
-}
-});
+   var days = getDaysInMonth(month, year)
+
+   days.forEach(function (el, i) {
+      var i = ++i + 0;
+      var id = 0
+      var num = `${i}`
+      var tab = `${i}`
+      var today = new Date()
+      var weekday = today.getDate();
+      var newdiv = '<div id=' + num + ' class=result> ' + tab + ' </div>';
+      document.getElementById('days').innerHTML += (newdiv);
+      if (tab == weekday) {
+         document.getElementById(weekday).classList.add('today')
+
+      }
+   });
 
 }
+document.addEventListener("DOMContentLoaded", main);
